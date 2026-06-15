d73899e30118d4c4d40a852f3e398a64e86ded5f
silbars <silbars22@gmail.com>
2026-06-15 11:33:37 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         while (true) { 
             System.out.print("$ ");
-            String command = sc.nextLine();
-            if (command.equals("exit")) break;
+            String command = sc.nextLine().trim();
+            String[] commandWords = command.split(" ", 2);
+            String builtinCommands = "echo exit type";
 
-            if(command.contains("echo")) {
-                String newCommand = command.replace("echo ", "");
+            if (command.equals("exit")) break;
+            else if(commandWords[0].equals("echo")) {
+                String newCommand = commandWords.length > 1 ? commandWords[1] : "";
                 System.out.println(newCommand);
+            } else if (commandWords[0].equals("type")) {
+                if(builtinCommands.contains(commandWords[1])) {
+                    System.out.println(commandWords[1] +" is a shell builtin");
+                } else {
+                    System.out.println(commandWords[1] + ": not found");
+                }
             } else {
                 System.out.println(command + ": command not found");    
             }
